783a8dd7aaffe4f506590c6bccbd50b6dde23025
Lucas Roseti <lc.roseti@gmail.com>
2024-03-16 17:20:31 -0300

    Style form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -17,15 +17,17 @@ export function Home() {
         Sexta 4, de março de 2024
       </Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6B6B6B"
-      />
-      
-      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
